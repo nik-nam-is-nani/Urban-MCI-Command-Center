0585a0db837346c118d07a172cf8209528f95f1f
nikshith <154665464+nik-nam-is-nani@users.noreply.github.com>
2026-04-03 11:39:23 +0530

    Change default port from 8000 to 7860

--- app.py
+++ app.py
@@ -351,5 +351,5 @@ def dashboard():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 7860))
     app.run(host='0.0.0.0', port=port, debug=False)
